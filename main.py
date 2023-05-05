8e4fd343156494d83bfb25d677acfde46c11135c
manolopepe777 <marcluchaluna79@gmail.com>
2023-05-05 16:53:11 +0200

    Contacto acabado

--- main.py
+++ main.py
@@ -2,25 +2,38 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+
 @app.route("/")
 @app.route("/inicio")
 def inici():
     return render_template("inicio.html")
 
+
 @app.route("/servicios")
 def servicios():
     return render_template("servicios.html")
 
-@app.route("/submit", methods=["POST"])
+
+@app.route("/submit_servicio", methods=["POST"])
 def submit():
-    global variable
-    variable = True
+    global variable1
+    variable1 = True
     nombre = request.form.get("nombre")
-    return render_template("enviado.html", nombre=nombre, variable=variable)
+    return render_template("enviadoServicio.html", nombre=nombre, variable1=variable1)
+
+
+@app.route("/submit_contacto", methods=["POST"])
+def submit2():
+    global variable2
+    variable2 = True
+    nombre = request.form.get("nombre")
+    return render_template("enviadoContacto.html", nombre=nombre, variable2=variable2)
+
 
 @app.route("/productos")
 def productos():
     return render_template("productos.html")
+
 
 @app.route("/contacto")
 def contacto():
